c07c73f70363fbdac66fa0ef5e7da8e09870be83
sokaraku <soner.karakus1999@gmail.com>
2024-02-29 16:05:55 +0100

    Freeing static's memory after reading in count_lines_fd

--- src/count_lines_fd.c
+++ src/count_lines_fd.c
@@ -6,7 +6,7 @@
 /*   By: sokaraku <sokaraku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:31:07 by sokaraku          #+#    #+#             */
-/*   Updated: 2024/02/29 15:58:07 by sokaraku         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:05:18 by sokaraku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,12 @@ int	count_lines_fd(int fd, char *path)
 		while (get_next_line(tmp_fd, 0))
 			i++;
 		close(tmp_fd);
+		get_next_line(fd, 1);
 		return (i);
 	}
 	while (get_next_line(fd, 0))
 		i++;
+	get_next_line(fd, 1);
 	close(fd);
 	return (i);
 }
