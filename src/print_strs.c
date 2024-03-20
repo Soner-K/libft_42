a1671338bde0937cf06e0f66e158357da954db91
sokaraku <soner.karakus1999@gmail.com>
2024-03-20 14:06:24 +0100

    wololololo

--- src/print_strs.c
+++ src/print_strs.c
@@ -6,7 +6,7 @@
 /*   By: sokaraku <sokaraku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 14:02:19 by sokaraku          #+#    #+#             */
-/*   Updated: 2024/03/20 14:04:19 by sokaraku         ###   ########.fr       */
+/*   Updated: 2024/03/20 14:06:13 by sokaraku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ void	print_strs(char **strs)
 
 	i = -1;
 	while (strs[++i])
-		ft_putstr_fd(strs[i], STDOUT_FILENO);
+		ft_putendl_fd(strs[i], STDOUT_FILENO);
 }
